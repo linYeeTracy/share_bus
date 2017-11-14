4385851a17f671a21bc380aab1dcdd33396457fd
linYeeTracy <494787963@qq.com>
2017-11-14 20:05:20 +0800

    引入百度地图

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from './App';
 import router from './router';
 import axios from 'axios';
 import ElementUI from 'element-ui';
+import BaiduMap from 'vue-baidu-map';
+
 import 'element-ui/lib/theme-default/index.css';    // 默认主题
 // import '../static/css/theme-green/index.css';       // 浅绿色主题
 import "babel-polyfill";
 
 Vue.use(ElementUI);
+Vue.use(BaiduMap, {   // 引入百度地图
+    ak: 'YOUR_APP_KEY'
+  })
 Vue.prototype.$axios = axios;
+
 new Vue({
     router,
     render: h => h(App)
